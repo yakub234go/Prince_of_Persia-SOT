c97d34901709169bdd47aa357e441ad40ebb3c59
yakub234go <yakub234go@gmail.com>
2014-07-15 21:35:04 +0530

    Initial commit

---
+++ Prince_of_Persia-SOT/SOT.Designer.cs
@@ -0,0 +1,352 @@
+﻿namespace Prince_of_Persia_SOT
+{
+    partial class SOT
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SOT));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.label15 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(1, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 391);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.tableLayoutPanel1);
+            this.groupBox1.Location = new System.Drawing.Point(307, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(328, 175);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label4, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label5, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label6, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label9, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label10, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label7, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.label8, 1, 4);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(6, 19);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 5;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(316, 150);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hotkey";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(161, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Effect(s)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(3, 29);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(21, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "F1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(161, 29);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(87, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Infinite Health";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.Black;
+            this.label5.Location = new System.Drawing.Point(3, 58);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(21, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "F2";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.Color.Black;
+            this.label6.Location = new System.Drawing.Point(161, 58);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(92, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Infinite Rewind";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.ForeColor = System.Drawing.Color.Black;
+            this.label9.Location = new System.Drawing.Point(3, 87);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(21, 13);
+            this.label9.TabIndex = 10;
+            this.label9.Text = "F3";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.ForeColor = System.Drawing.Color.Black;
+            this.label10.Location = new System.Drawing.Point(161, 87);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(119, 13);
+            this.label10.TabIndex = 11;
+            this.label10.Text = "Infinite Slow-Motion";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(3, 116);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(21, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "F4";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(161, 116);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(94, 13);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "Back to Normal";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(307, 362);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(245, 24);
+            this.label11.TabIndex = 2;
+            this.label11.Text = "The game is not running!";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(307, 193);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(328, 45);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Trainer Instructions";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(310, 241);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(261, 15);
+            this.label12.TabIndex = 4;
+            this.label12.Text = "Trainer by Yakub Khan aka MostWanted";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(310, 256);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(174, 15);
+            this.label13.TabIndex = 5;
+            this.label13.Text = "(yakub234go@gmail.com)";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label14.Location = new System.Drawing.Point(307, 280);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(60, 15);
+            this.label14.TabIndex = 6;
+            this.label14.Text = "Credits :";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel1.Location = new System.Drawing.Point(307, 297);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(93, 15);
+            this.linkLabel1.TabIndex = 9;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Matej Tomčík";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel2.Location = new System.Drawing.Point(307, 312);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(129, 15);
+            this.linkLabel2.TabIndex = 10;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "CheatEngine Team";
+            this.linkLabel2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel2_LinkClicked);
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label15.Location = new System.Drawing.Point(304, 336);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(174, 17);
+            this.label15.TabIndex = 11;
+            this.label15.Text = "Thanks to the Almighty";
+            // 
+            // SOT
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(647, 391);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.linkLabel2);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "SOT";
+            this.Opacity = 0.85D;
+            this.Text = "Prince of Persia - The Sands of Time";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.SOT_FormClosing);
+            this.Load += new System.EventHandler(this.SOT_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+        private System.Windows.Forms.Label label15;
+    }
+}
+
---
+++ Prince_of_Persia-SOT/SOT.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
+using System.Media;
+
+namespace Prince_of_Persia_SOT
+{
+    public partial class SOT : Form
+    {
+        public SOT()
+        {
+            InitializeComponent();
+            timer1.Start();
+        }
+
+        SoundPlayer splay;
+        Process[] processes;
+        public void inf_h()
+        {
+            using (CheatEngine.Memory memory = new CheatEngine.Memory(processes[0]))
+            {
+                try
+                {
+                    IntPtr address1 = memory.GetAddress("POP.exe", (IntPtr)0x006DD0C8, new int[] { 0x33c, 0x234, 0x44, 0x19c });
+                    uint health = 200;
+                    memory.WriteUInt32(address1, health);
+                }
+                catch (SystemException se)
+                {
+                    Debug.WriteLine(se.ToString());
+                }
+            }
+        }
+
+        public void inf_r()
+        {
+            using (CheatEngine.Memory memory = new CheatEngine.Memory(processes[0]))
+            {
+                try
+                {
+                    IntPtr address1 = memory.GetAddress("POP.exe", (IntPtr)0x006DB4DC, new int[] { 0x30 });
+                    IntPtr address2 = memory.GetAddress("POP.exe", (IntPtr)0x006EE74C, new int[] { 0x214, 0x19c, 0x218 });
+                    uint rewindcloud = 10;
+                    uint rewind = 9000000;
+                    memory.WriteUInt32(address1, rewindcloud);
+                    memory.WriteUInt32(address2, rewind);
+                }
+                catch (SystemException se)
+                {
+                    Debug.WriteLine(se.ToString());
+                }
+            }
+        }
+
+        public void inf_s()
+        {
+            using (CheatEngine.Memory memory = new CheatEngine.Memory(processes[0]))
+            {
+                try
+                {
+                    IntPtr address1 = memory.GetAddress("POP.exe", (IntPtr)0x006DB8A4, new int[] { 0x0 });
+                    IntPtr address2 = memory.GetAddress("POP.exe", (IntPtr)0x006DB0F0, new int[] { 0x0 });
+                    uint slowmotiontank = 10;
+                    uint slowmotion = 0;
+                    memory.WriteUInt32(address1, slowmotiontank);
+                    memory.WriteUInt32(address2, slowmotion);
+                }
+                catch (SystemException se)
+                {
+                    Debug.WriteLine(se.ToString());
+                }
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            processes = Process.GetProcessesByName("POP");
+            if (processes.Length == 0)
+            {
+                label11.Text = "The game is not running!";
+                label11.ForeColor = Color.Red;
+                label3.ForeColor = Color.Black;
+                label4.ForeColor = Color.Black;
+                label5.ForeColor = Color.Black;
+                label6.ForeColor = Color.Black;
+                label9.ForeColor = Color.Black;
+                label10.ForeColor = Color.Black;
+                ghook.unhook();
+            }
+            else if (processes.Length > 0)
+            {
+                label11.ForeColor = Color.Green;
+                label11.Text = "The game is running";
+                if (label4.ForeColor==Color.Red)
+                {
+                    inf_h();
+                }
+                if (label6.ForeColor == Color.Red)
+                {
+                    inf_r();
+                }
+                if (label10.ForeColor == Color.Red)
+                {
+                    inf_s();
+                }
+            }
+        }
+
+        GlobalKeyboardHook ghook;
+        private void SOT_Load(object sender, EventArgs e)
+        {
+            ghook = new GlobalKeyboardHook();
+            ghook.KeyDown += new KeyEventHandler(ghook_keydown);
+            ghook.HookedKeys.Add(Keys.F1);
+            ghook.HookedKeys.Add(Keys.F2);
+            ghook.HookedKeys.Add(Keys.F3);
+            ghook.HookedKeys.Add(Keys.F4);
+            ghook.hook();
+        }
+
+        string hotkey;
+        public void ghook_keydown(object sender, KeyEventArgs e)
+        {
+            hotkey = e.KeyCode.ToString();
+            if (hotkey == "F1")
+            {
+                if (label4.ForeColor == Color.Black)
+                {
+                    label3.ForeColor = Color.Red;
+                    label4.ForeColor = Color.Red;
+                }
+                else if (label4.ForeColor == Color.Red)
+                {
+                    label3.ForeColor = Color.Black;
+                    label4.ForeColor = Color.Black;
+                }
+                System.Media.SystemSounds.Asterisk.Play();
+            }
+            if (hotkey == "F2")
+            {
+                if (label6.ForeColor == Color.Black)
+                {
+                    label5.ForeColor = Color.Red;
+                    label6.ForeColor = Color.Red;
+                }
+                else if (label6.ForeColor == Color.Red)
+                {
+                    label5.ForeColor = Color.Black;
+                    label6.ForeColor = Color.Black;
+                }
+                System.Media.SystemSounds.Asterisk.Play();
+            }
+            if (hotkey == "F3")
+            {
+                if (label10.ForeColor == Color.Black)
+                {
+                    label9.ForeColor = Color.Red;
+                    label10.ForeColor = Color.Red;
+                }
+                else if (label10.ForeColor == Color.Red)
+                {
+                    label9.ForeColor = Color.Black;
+                    label10.ForeColor = Color.Black;
+                }
+                System.Media.SystemSounds.Asterisk.Play();
+            }
+            if (hotkey == "F4")
+            {
+                foreach (Label l in tableLayoutPanel1.Controls)
+                {
+                    l.ForeColor = Color.Black;
+                }
+            }
+            System.Media.SystemSounds.Asterisk.Play();
+        }
+
+        private void SOT_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            ghook.unhook();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Run the game, Run the trainer\nLoad the game\nPress Hotkey for desired cheat\n\nThank You & Happy Gaming");
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("http://www.youtube.com/watch?v=TWUK-fK7xD8");
+        }
+
+        private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("http://www.cheatengine.org/");
+        }
+    }
+}
